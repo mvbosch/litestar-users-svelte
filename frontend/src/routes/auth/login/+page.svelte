540f1a1423276961a7d518ea86a1778e2623c538
mvbosch <michael@vonbosch.co.za>
2025-04-21 10:46:39 +0000

    feat: login page

---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Login from '$domain/users/forms/Login.svelte';
+</script>
+
+<div>
+  <h1>Login</h1>
+  <form-container>
+    <Login />
+    <p>Don't have an account? <a href="/auth/register">Register here</a></p>
+    <p><a href="/auth/forgot-password">Forgot Password?</a></p>
+  </form-container>
+</div>
+
+<style>
+  p {
+    margin-top: 1rem;
+  }
+  form-container {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    align-items: center;
+    justify-items: center;
+  }
+
+</style>
