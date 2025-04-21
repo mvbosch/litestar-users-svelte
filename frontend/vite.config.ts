5a214879670ebb0a40fb1fb12590d4569540a325
mvbosch <michael@vonbosch.co.za>
2025-04-21 10:42:44 +0000

    build: set host and port for devcontainer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 3000,
+		strictPort: true,
+		host: '0.0.0.0',
+	},
 });
